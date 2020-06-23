50cf5e7c872ab992a59efcd4cb62ebc0226e09a1
Simen <Simen.Carstensen@student.uib.no>
2020-06-23 12:19:45 +0200

    Completed String Matching

--- src/StringMatching.java
+++ src/StringMatching.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringMatching {
     public static void main(String[] args) throws IOException {
@@ -11,9 +9,59 @@ public class StringMatching {
         String pattern;
         while((pattern = reader.readLine()) != null) {
             String text = reader.readLine();
-            Pattern patt = Pattern.compile(pattern);
-            Matcher matcher = patt.matcher(text);
-            System.out.println(matcher.matches());
+            kmpSearch(text, pattern);
+            System.out.println();
+        }
+
+        reader.close();
+    }
+    public static void kmpSearch(String txt, String pat) {
+        int M = pat.length();
+        int N = txt.length();
+        int lps[] = new int[M];
+        int j = 0;
+        int i = 0;
+        computeLPSArray(pat, M, lps);
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.print(i-j + " ");
+                j = lps[j - 1];
+            }
+
+
+            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                }
+                else {
+                    i = i + 1;
+                }
+            }
+        }
+    }
+    public static void computeLPSArray(String pat, int M, int lps[]) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = len;
+                    i++;
+                }
+            }
         }
     }
 }
