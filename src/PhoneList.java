f196080bf38871ba4310bff1aa7e580bcaaa17b9
sca003 <simencarstensen@gmail.com>
2020-06-30 00:17:12 +0200

    completed "phone list"

--- src/PhoneList.java
+++ src/PhoneList.java
@@ -1,24 +1,28 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class PhoneList {
     public static void main(String[] args) {
         Kattio io = new Kattio(System.in, System.out);
         int cases = io.getInt();
-        for(int i = 0; i < cases; i++) {
+        for (int i = 0; i < cases; i++) {
             int N = io.getInt();
             String[] nums = new String[N];
-            HashMap<String, Integer> map = new HashMap<>();
-            for(int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++) {
                 String num = io.getWord();
                 nums[j] = num;
-                map.put(num,0);
             }
+            Arrays.sort(nums);
             boolean prefix = false;
-            
+            for (int j = 1; j < N; j++) {
+                if (nums[j].startsWith(nums[j - 1])) {
+                    prefix = true;
+                    break;
+                }
+
+            }
             if (prefix) {
                 io.println("NO");
-            }
-            else {
+            } else {
                 io.println("YES");
             }
         }
