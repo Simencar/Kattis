656db5ae6d426e2d44e898bcf1d28255f05cb632
Simen <Simen.Carstensen@student.uib.no>
2020-07-01 00:18:31 +0200

    solved fizzbuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,21 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        
+        Kattio io = new Kattio(System.in, System.out);
+        int x = io.getInt();
+        int y = io.getInt();
+        int n = io.getInt();
+        for(int i = 1; i <= n; i++) {
+            if(i%x == 0) {
+                io.print("Fizz");
+            }
+            if(i%y == 0) {
+                io.print("Buzz");
+            }
+            if(i%x != 0 && i%y != 0) {
+                io.print(i);
+            }
+            io.println();
+        }
+        io.close();
     }
 }
