4ce6173b4e626742cdeb5fba7e31af210091d5d8
sca003 <simencarstensen@gmail.com>
2020-07-04 13:09:35 +0200

    test vscode commit

--- cpp/BankQueue.cpp
+++ cpp/BankQueue.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int N, T;
-cin >> N >> T;
-int money [N];
-int time [N];
-for(int i = 0; i < N; i++) {
-    int m, t;
-    cin >> m >> t;
-    money[i] = m;
-    time[i] = t;
-}
-
-
+    int N, T;
+    cin >> N >> T;
+    int money[N];
+    int time[N];
+    for (int i = 0; i < N; i++)
+    {
+        int m, t;
+        cin >> m >> t;
+        money[i] = m;
+        time[i] = t;
+    }
 }
