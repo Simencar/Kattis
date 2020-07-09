9419813463360e7e2239852f10a29246e6560f0f
sca003 <simencarstensen@gmail.com>
2020-07-09 21:18:52 +0200

    started shortest path

---
+++ cpp/SingleSourceShortestPath.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, q, s;
+    while((cin >> n >> m >> q >> s) && n != 0 && m != 0 && q != 0 && s != 0) {
+        int unvisited[n];
+    }
+
+
+
+    return 0;
+}
