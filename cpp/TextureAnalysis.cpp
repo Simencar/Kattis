1ee065f94ebbbf480165513ad2593fd94e7cdccf
Simen <Simen.Carstensen@student.uib.no>
2020-07-09 14:32:07 +0200

    completed texture analysis

--- cpp/TextureAnalysis.cpp
+++ cpp/TextureAnalysis.cpp
@@ -6,12 +6,15 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     string line;
+    int N = 1;
     while ((cin >> line) && line != "END")
     {
         bool even = true;
         int blackDist = 0;
+
         if (line.length() > 1)
         {
+
             for (int i = 1; i < line.length(); i++)
             {
                 if (line.at(i) == '*')
@@ -20,25 +23,40 @@ int main()
                     break;
                 }
             }
-            for (int i = 0; i < line.length(); i += blackDist)
+
+            int count = 1;
+            for (int i = blackDist + 1; i < line.length(); i++)
             {
-                cout << i << endl;
-                if (line.at(i) != '*')
+                if (count < blackDist)
                 {
-                    even = false;
-                    break;
+                    if (line.at(i) != '.')
+                    {
+                        even = false;
+                        goto print;
+                    }
+                    count++;
+                }
+                else if (count == blackDist)
+                {
+                    if (line.at(i) != '*')
+                    {
+                        even = false;
+                        goto print;
+                    }
+                    count = 1;
                 }
             }
         }
-
+    print:
         if (even)
         {
-            cout << "EVEN" << endl;
+            cout << N << " EVEN" << endl;
         }
         else
         {
-            cout << "NOT EVEN" << endl;
+            cout << N << " NOT EVEN" << endl;
         }
+        N++;
     }
 
     return 0;
