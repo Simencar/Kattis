ebe0bf3d1b8dc5079dab8d4fb66d5ce6fc99bdcc
Simen <Simen.Carstensen@student.uib.no>
2020-07-21 17:17:30 +0200

    completed union find

--- cpp/UnionFind.cpp
+++ cpp/UnionFind.cpp
@@ -1,22 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int parent[], int i)  
-{  
-    if (parent[i] == -1)  
-        return i;  
-    return find(parent, parent[i]);  
-} 
+class QuickUnion {
+    public:
+    int* id;
+    int* size;
 
-void Union(int parent[], int x, int y)  
-{  
-    int xset = find(parent, x);  
-    int yset = find(parent, y);  
-    if(xset != yset) 
-    {  
-        parent[xset] = yset;  
-    }  
-} 
+    QuickUnion(int n) {
+        id = new int[n];
+        size = new int[n];
+        for(int i = 0; i < n; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int find(int p) {
+        int root = p;
+        while(root != id[root]) {
+            id[root] = id[id[root]];
+            root = id[root];
+        }
+        return root;
+    }
+
+    bool connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    void Union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if(rootP == rootQ) {
+            return;
+        }
+        if(size[rootP] < size[rootQ]) {
+            id[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else{
+            id[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -25,29 +54,29 @@ int main() {
     int N, Q;
     cin >> N >> Q;
 
-    int *parent = new int[N * sizeof(int)];  
-    memset(parent, -1, N * sizeof(int));
+    QuickUnion * qU = new QuickUnion(N);
 
     for(int i = 0; i < Q; i++) {
         string op;
         int a, b;
         cin >> op >> a >> b;
 
-        if (op == "?") {
-            if(a == b || (parent[a] == parent[b] && parent[a] != -1) || parent[a] == b || parent[b] == a) {
-                cout << "yes" << endl;
-            }
-
-            else {
-                cout << "no" << endl;
-            }
+        if (op == "=") {
+            qU->Union(a,b);
         }
 
         else {
-            Union(parent, a, b);
+            if(qU->connected(a,b)) {
+                cout << "yes\n"; //DONT USE endl; SPENT HOURS DEBUGGING TIMELIMIT EXCEEDING 
+            }
+
+            else {
+                cout << "no\n";
+            }
         }
     }
 
 
     return 0;
 }
+
