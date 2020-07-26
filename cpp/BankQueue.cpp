2617b49dc51f3f218456b2c9cbbf1f94d34bc811
Simen <Simen.Carstensen@student.uib.no>
2020-07-26 22:55:32 +0200

    not completed

--- cpp/BankQueue.cpp
+++ cpp/BankQueue.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int N, T;
     cin >> N >> T;
-    int money[N];
-    int time[N];
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         int m, t;
         cin >> m >> t;
-        money[i] = m;
-        time[i] = t;
-        //hei
+        
     }
 }
