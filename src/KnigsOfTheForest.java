972e2fd1407d426d8260d751fecbbdf2c795543c
Simen <Simen.Carstensen@student.uib.no>
2020-12-10 16:04:42 +0100

    First draft for Kings of the forest

--- src/KnigsOfTheForest.java
+++ src/KnigsOfTheForest.java
@@ -1,15 +1,39 @@
-import WheresMyInternet.Kattio;
+
+import java.util.PriorityQueue;
 
 public class KnigsOfTheForest {
 
     public static void main(String[] args) {
         Kattio io = new Kattio(System.in, System.out);
+        PriorityQueue<Moose> pq = new PriorityQueue<>();
         int k = io.getInt();
         int n = io.getInt();
-
+        Moose karl = new Moose(io.getInt(), io.getInt());
+        pq.add(karl);
+        for (int i = 0; i < n+k-2; i++) {
+            Moose m = new Moose(io.getInt(), io.getInt());
+            pq.add(m);
+        }
+        String out = "unknown";
+        while(pq.size() > 1) {
+            Moose winner = pq.poll();
+            if (winner.str == karl.str) {
+                out = Integer.toString(winner.year);
+                break;
+            }
+            for(int i = 0; i < k-1; i++) {
+                Moose oldM = pq.poll();
+                Moose newM = new Moose(oldM.year+1, oldM.str);
+                pq.add(newM);
+            }
+        }
+        io.println(out);
+        io.close();
     }
 }
-class Moose {
+
+
+class Moose implements Comparable<Moose> {
     public int year;
     public int str;
 
